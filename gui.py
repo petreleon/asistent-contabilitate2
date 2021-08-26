d91b59fd2e753e232549181d470057ee339eecbe
Petre Leonard Macamete <petreleonardos@gmail.com>
2021-08-26 06:55:28 +0300

    changes

--- gui.py
+++ gui.py
@@ -24,7 +24,7 @@ from tkinter.filedialog import asksaveasfilename, askopenfilename
 
 root = Tk()
 root.geometry('200x200')
-  
+
 # function to call when user press
 # the save button, a filedialog will
 # open and ask to save file
@@ -35,7 +35,7 @@ def texting(text_change: typing.Union[str, NoneType] = None):
     if text_change is not None:
         text.insert('1.0', text_change)
 
-
+collumn_names = dict()
 
 def save():
     files = [('All Files', '*.*'),
@@ -43,10 +43,24 @@ def save():
     file = asksaveasfilename(filetypes = files, defaultextension = files)
     print(file)
 
+def getFromRow(row, col_enum):
+    return row[collumn_names[col_enum.value]].value
+
+lastNumber = 0 # will be modified further
+externalCodes = dict()
+companiesToQuery = list()
+
+def addCompany(CUI, externalCode):
+    pass
+
+def iterOnRows(rows):
+    second_row = rows[2]
+    last_doc = getFromRow(second_row, colNames.NUMAR_FACTURA)
+
 
 def processing(file):
     global inputtxt
-    collumn_names = dict()
+    global lastNumber
     lastCode = inputtxt.get("1.0", END)
     print(lastCode)
     lastNumber = int(lastCode[1:].lstrip("0"))
@@ -56,8 +70,8 @@ def processing(file):
     first_row = currentSheet[1]
     for index, collumn in enumerate(first_row):
        collumn_names[collumn.value] = index
+    iterOnRows(currentSheet)
     
-
 
 
 def open():
